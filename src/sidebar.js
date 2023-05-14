cc3c78ac8a766c94858b5f01d1dae4b34fd72205
nothing331 <ayush33156@gmail.com>
2023-05-14 21:24:27 +0530

    3rd commit
    sidebar, pfp , icons, search bar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import "./Sidebar.css";
-
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Avatar, IconButton } from '@mui/material';
+import { SearchOutlined } from '@mui/icons-material';
 function Sidebar() {
   return (
     <div className ='sidebar'>
-      <h1>I am a sidebar</h1>
-      <div className='sidebbar_header'>
-        
+      <div className='sidebar_Lower'>
+        <Avatar src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Featured-Image-Luffy.jpg" />
+        <div className='sidebar_lower_right'>
+          <IconButton>
+            <MicIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon/>
+          </IconButton>
+          <IconButton>
+            <HeadphonesIcon/>
+          </IconButton>
+        </div>
+      </div>
+      <div className='search_bar'>
+        <div className='search_container'>
+          <SearchOutlined />
+          <input placeholder='Search friend here' type='text' />
+        </div>
       </div>
     </div>
   )
