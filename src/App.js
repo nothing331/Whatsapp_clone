6fb29f053daabf385310f549553955f09f586baf
nothing331 <ayush33156@gmail.com>
2023-05-14 16:34:51 +0530

    First Commit
    made sidebar and chat structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Chat from './Chat';
+import Sidebar from './sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='app_body'>
+      <Sidebar />
+      <Chat/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className ='sidebar'>
+      <h1>I am a sidebar</h1>
+      <div className='sidebbar_header'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
